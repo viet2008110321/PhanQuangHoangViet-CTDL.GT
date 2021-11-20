bd132df398c0d31f291e892844e2a8305b27dd79
viet2008110321 <0364959576viet@gamil.com>
2021-11-20 16:46:14 +0700

    Cow + testdrive

---
+++ src/vn/edu/giadinh/tuan03/Cow.java
@@ -0,0 +1,10 @@
+package vn.edu.giadinh.tuan03;
+
+public class Cow {
+    double weight;
+    
+    Cow(double w){
+        weight = w;
+        
+    }
+}
---
+++ src/vn/edu/giadinh/tuan03/CowTestDrive.java
@@ -0,0 +1,12 @@
+package vn.edu.giadinh.tuan03;
+
+public class CowTestDrive {
+    public static void main(String[] args) {
+        Cow cow;
+        cow = new Cow(15);
+        System.out.println("wweight: " + cow.weight);
+
+        cow.weight = 15;
+
+    }
+}
